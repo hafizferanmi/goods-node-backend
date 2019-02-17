65ed4e562fb446517a14123ef1c3d76404b30538
hafiz feranmi <hafizferanmi@gmail.com>
2019-02-17 17:42:32 +0100

    made it better

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT;
 var router = express.Router();
-mongoose.connect(process.env.DB);
-// mongoose.connect('mongodb://hf:123hafizferanmi@ds239055.mlab.com:39055/goods');
+// mongoose.connect(process.env.DB);
+mongoose.connect('mongodb://hf:123hafizferanmi@ds239055.mlab.com:39055/goods');
 
 
 router.get('/', function(req, res) {
